351eacf3b6e0fc05ddc1449018daa42733a2b78c
unknown <sandeepkumarp9229@gmail.com>
2024-04-29 21:01:47 +0530

    adding

---
+++ src/main/java/com/cicd/CicdSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdSpringBootApplication.class, args);
+	}
+
+}
